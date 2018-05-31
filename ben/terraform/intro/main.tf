da36c303a06896e458eea31c25c96bb062974934
Ben Gnoinski <ben.gnoinski@gmail.com>
2018-05-30 19:46:45 -0700

    Added terraform intro post Added terraform intro test code updated gitignore to ignore .terraform and .tfstate

---
+++ ben/terraform/intro/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "ca-central-1"
+}
+
+# Initial vpc block, you should plan and apply this one
+
+resource "aws_vpc" "dev_vpc" {
+    cidr_block = "10.0.0.0/24"
+}
+
+# Update in place
+/*
+resource "aws_vpc" "dev_vpc" {
+    cidr_block = "10.0.0.0/24"
+
+    tags {
+        Name = "dev_vpc"
+    }
+}
+*/
+
+# Destroy and re-create
+/*
+resource "aws_vpc" "dev_vpc" {
+    cidr_block = "10.10.0.0/16"
+
+    tags {
+        Name = "dev_vpc"
+    }
+}
+*/
